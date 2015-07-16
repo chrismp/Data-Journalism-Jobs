fc423cbcdb24df2796e8d9348ea502635ad77c27
unknown <cpersaud@DEV-CPERSAUD-US.cortera.local>
2015-07-16 16:28:22 -0400

    starting to add JS for making listings table

--- main.rb
+++ main.rb
@@ -16,12 +16,15 @@ end
 get '/jobs' do 
 	returnArray = []
 	newGSheet = GSheet.new
-	# newGSheet.sheetId = ENV['JOBS_GSHEET_ID']
-	newGSheet.sheetId = '1kDpKG0XhlbM679tfZe3pWtxmvnAIM_NZY7aNU5jFlBg'
+	newGSheet.sheetId = ENV['JOBS_GSHEET_ID']
 	jobListingDataArray = newGSheet.showData
 	jobListingDataArray.each{|jobData|
 		g = 'gsx$'
 		t = '$t'
+
+		if(jobData[g+'closed'][t].to_i===1 && jobData[g+'approved'][t].to_i!=1)
+			next
+		end
 
 		returnHash = {
 			:jobTitle => jobData[g+'jobtitle'][t],
@@ -29,11 +32,11 @@ get '/jobs' do
 			:jobDescription => jobData[g+'jobdescription'][t],
 			# :skillsArray => jobData[g+'skillsrequired'][t].split(',').map{|skill| skill.strip},
 			:skills => jobData[g+'skillsrequired'][t],
-			:location => jobData[g+'location'][t],
-			:url => jobData[g+'websiteurl'][t],
+			:jobLocation => jobData[g+'location'][t],
+			:companyURL => jobData[g+'websiteurl'][t],
 			:apply => jobData[g+'howtoapply'][t],
-			:reqEducation => jobData[g+'minimumeducationrequired'][t],
-			:reqYearsExp => jobData[g+'minimumyearsofexperiencerequired'][t],
+			:education => jobData[g+'minimumeducationrequired'][t],
+			:yearsExp => jobData[g+'minimumyearsofexperiencerequired'][t],
 			:internship => jobData[g+'isthisaninternship'][t],
 			:closed => jobData[g+'closed'][t],
 			:approved => jobData[g+'approved'][t]
--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,42 @@
 $(function(){
+	// var tr = 'tr';
+	// var th = 'th';
+	var $listings = $('#listings');
+	var headersArray = [
+		'Job',
+		'Company',
+		'Location',
+		'Skills',
+		'Education',
+		'Years of experience',
+		'Job description'
+	];
+	
+	$listings.append('<tr id="header-row"></tr>');
+	var $headerRow = $('#header-row');
+	
+	for (var i=0; i<headersArray.length; i++) {
+		var header = headersArray[i];
+		$headerRow.append('<th>'+header+'</th>');	
+	}
+
 	$.get(
 		'/jobs',
 		function(data){
-			console.log(data);
+			var jobsDataArray = JSON.parse(data);
+			for (var i=0; i<jobsDataArray.length; i++) {
+				var job = jobsDataArray[i];
+				var tdArray = [
+					job.jobTitle,
+					'<a href="'+job.companyURL+'" target="_blank">'+job.company+'</a>',
+					job.jobLocation,
+					job.skills,
+					job.education,
+					job.yearsExp,
+					job.jobDescription
+				];
+				$listings.append()
+			}
 		}
-	);
+	)
 })
